32fabce078df6e7613f5ff3b3c2411ddf4d80d2c
Shad Sharma <shadanan@gmail.com>
2010-11-28 21:35:43 -0600

    deleted temp files

--- pc12.py
+++ pc12.py
@@ -1,64 +1,22 @@
 #!/usr/bin/env python
 # encoding: utf-8
-"""
-pc12.py
 
-Created by Shadanan Sharma on 2010-04-22.
-"""
-
-import sys
 import os
-import math
+import Image
 
-def factor(n):
-    for i in xrange(1, int(math.sqrt(n))):
-        if n % i == 0:
-            print i, n/i
+fp = open("evil2.gfx", "rb")
+data = fp.read()
+fp.close()
 
-def partition(data, n):
-    result = []
-    for i in xrange(0, len(data), n):
-        result.append(tuple(data[i:i+n]))
-    return result
+files = []
+for i in range(5):
+    files.append(open('/tmp/pc12_image_%d.gif' % i, 'wb'))
 
-def shuffle(data):
-    result = []
-    for i in xrange(0, len(data), 5):
-        result.append(data[i+4])
-        result.append(data[i+3])
-        result.append(data[i+1])
-        result.append(data[i])
-        result.append(data[i+2])
-    return result
+for index, val in enumerate(data):
+    files[index % 5].write(val)
 
-def flatten(data):
-    result = []
-    for i in data:
-        result += i
-    return result
+for f in files:
+    f.close()
 
-def pick(data, k, n):
-    result = []
-    for i in xrange(len(data)):
-        if i % n in k:
-            result.append(data[i])
-    return result
-
-def main():
-    fp = open("evil2.gfx", "rb")
-    data = fp.read()
-    fp.close()
-    
-    files = []
-    for i in range(5):
-        files.append(open('file%d.gif' % i, 'wb'))
-    
-    for index, val in enumerate(data):
-        files[index % 5].write(val)
-    
-    for f in files:
-        f.close()
-    
-if __name__ == '__main__':
-    main()
-
+for i in range(5):
+    os.system('open /tmp/pc12_image_%d.gif' % i)
