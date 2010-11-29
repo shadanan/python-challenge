5404b1a7f203e201d94709d494db983e9f542212
Shad Sharma <shadanan@gmail.com>
2010-11-28 21:26:45 -0600

    fixed pc11

--- pc11.py
+++ pc11.py
@@ -2,5 +2,18 @@
 
 import Image
 
-im = Image.open('cave.jpg')
-im.show()
+src_image = Image.open('cave.jpg')
+src_data = src_image.getdata()
+
+dest_data = []
+for index, value in enumerate(src_data):
+    if (index / 640) % 2 == 0:
+        if index % 2 == 0:
+            dest_data.append(value)
+    else:
+        if index % 2 == 1:
+            dest_data.append(value)
+
+dest_image = Image.new('RGB', (640, 320))
+dest_image.putdata(dest_data)
+dest_image.show()
