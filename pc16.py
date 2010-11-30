7e0f8883cb3d0a94a466ef6b3b999768aa90938b
Shad Sharma <shadanan@gmail.com>
2010-11-30 09:34:29 -0600

    finished level 16

--- pc16.py
+++ pc16.py
@@ -1,3 +1,28 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+import Image
+src = Image.open('mozart.gif')
+dest = Image.new('P', (1500, 500))
+dest.putpalette(src.getpalette())
+
+counter = 0
+start_points = []
+posx, posy = 0, 0
+
+for index, pixel in enumerate(src.getdata()):
+    dest.putpixel((posx, posy), pixel)
+    
+    if counter == 5:
+        start_points.append(index - 5)
+        posy += 1
+        posx = 0
+    
+    if pixel == 195:
+        counter += 1
+    else:
+        counter = 0
+    
+    posx += 1
+
+dest.show()
